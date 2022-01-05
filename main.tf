26b4a9e5449377e8140493f8fd1fac8960c393fd
Naresh Kumar G <93704634+Gunamoni11@users.noreply.github.com>
2022-01-05 23:10:53 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -7,5 +7,5 @@ module "slave" {
   module "master" {
     source = "git::https://github.com/Gunamoni11/demo.git?ref=master"
     security_groups = var.security_groups
-      target_group_arns = [module.slave.TG_ARN]
+  
  }
