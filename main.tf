d23d49322cc167d79a35eac6baf5c41d67c8b5c1
Naresh Kumar G <93704634+Gunamoni11@users.noreply.github.com>
2022-01-05 21:57:35 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,10 +2,10 @@ provider "aws"{
   region = "us-east-2"
 }
 module "A" {
-  source = "git::https://github.com/Ramana4u/test1.git?ref=slave"
+  source = "git::https://github.com/Gunamoni11/demo.git?ref=slave"
 }
   module "B" {
-    source = "git::https://github.com/Ramana4u/test1.git?ref=master"
+    source = "git::https://github.com/Gunamoni11/demo.git?ref=master"
     security_groups = var.security_groups
       target_group_arns = [module.A.TG_ARN]
  }
