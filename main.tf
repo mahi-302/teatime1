cab923bc08e8f8491a6527ec62ddc26e847bb22e
BODA MAHESH KUMAR <93734751+mahi-302@users.noreply.github.com>
2022-01-06 23:46:11 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,9 +2,9 @@ provider "aws"{
   region = "us-east-2"
 }
 module "a" {
-  source = "git::https://github.com/Gunamoni11/demo.git?ref=slave"
+  source = "git::https://github.com/mahi-302/teatime1.git?ref=slave"
 }
   module "b" {
-    source = "git::https://github.com/Gunamoni11/demo.git?ref=master"
+    source = "git::https://github.com/mahi-302/teatime1.git?ref=master"
     target_group_arns = module.a.TG_ARN
  }
