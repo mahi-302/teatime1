bb6d3ee4cc787fa570d18264fc933902b68f520f
Naresh Kumar G <93704634+Gunamoni11@users.noreply.github.com>
2022-01-06 20:12:00 +0530

    Update modulesg.tf

--- modulesg.tf
+++ modulesg.tf
@@ -1,4 +1,4 @@
-resource "aws_launch_configuration" "as_conf" {
+resource "aws_launch_configuration" "as_config" {
   name_prefix   = var.name_prefix
   image_id      = var.image_id
   instance_type = var.instance_type
@@ -7,7 +7,7 @@ resource "aws_launch_configuration" "as_conf" {
 }
 resource "aws_autoscaling_group" "bar" {
   name                 = var.name 
-  depends_on           = ["aws_launch_configuration.as_conf","aws_lb_target_group.test"]
+  depends_on           = ["aws_launch_configuration.as_config","aws_lb_target_group.test"]
   launch_configuration = aws_launch_configuration.as_conf.name
   min_size             = 1
   max_size             = 2
